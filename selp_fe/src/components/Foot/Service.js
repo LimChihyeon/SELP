1739ac5857012ccfc2055a4097715bb123adfa50
mkchoii <kkungrr@gmail.com>
2024-01-27 01:32:29 +0900

    FEAT: Footer Service button 추가

--- selp_fe/src/components/Foot/Service.js
+++ selp_fe/src/components/Foot/Service.js
@@ -12,13 +12,12 @@ const Service = () => {
             <div className="service_time">운영시간 평일 10:00 - 18:00 (토/일, 공휴일 휴무)<br/>점심시간 평일 13:00 - 14:00
             </div>
             <p className="service_notice">1:1 문의하기는 앱에서만 가능합니다.</p>
-            <div className="service_btn_box">
-                <Link to="/" className="btn solid small">
+            <Link to="/">
+                <button className="service_btn">
                     자주 묻는 질문
-                </Link>
-            </div>
+                </button>
+            </Link>
         </div>
-        
     );
 };
 
