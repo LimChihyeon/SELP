dfe52308c2ff1dd010fdf8cff791872d5c49482a
mkchoii <kkungrr@gmail.com>
2024-01-26 18:13:07 +0900

    FEAT: Footer Service 구현

--- selp_fe/src/components/Foot/Footer.js
+++ selp_fe/src/components/Foot/Footer.js
@@ -1,37 +1,15 @@
 import React from "react";
-import "../styles/App.css";
+import "styles/App.css";
+import Service from "./Service";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="footer.sm">
+      <div className="footer">
         <div className="inner">
-          <div className="service_area">
-            <div className="customer_service">
-              <strong className="service_title">
-                고객센터
-                <a href="tel:1588-7813" className="service_tel">
-                  1588-7813
-                </a>
-              </strong>
-              <div className="service_time">
-                <dl className="time_box">
-                  <dt className="time_term">
-                    운영시간 평일 10:00 - 18:00 (토/일, 공휴일 휴무) 점심시간
-                    평일 13:00 - 14:00
-                  </dt>
-                </dl>
-              </div>
-              <p className="service_notice">
-                1:1 문의하기는 앱에서만 가능합니다.
-              </p>
-              <div className="service_btn_box">
-                <a href="#" className="btn solid small">
-                  {" "}
-                  자주 묻는 질문{" "}
-                </a>
-              </div>
-            </div>
+          
+            <Service />
+            
             <div className="footer_menu">
               <div className="menu_box">
                 <strong className="menu_title">이용안내</strong>
@@ -143,7 +121,6 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-      </div>
     </footer>
   );
 };
--- selp_fe/src/index.js
+++ selp_fe/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Header from './components/Head/Header';
-import Footer from './components/Footer';
+import Footer from './components/Foot/Footer';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
