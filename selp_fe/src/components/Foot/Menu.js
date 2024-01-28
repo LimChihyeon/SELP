aa3d430ecd406e85548f6bf8b447d38dfdc4ad69
mkchoii <kkungrr@gmail.com>
2024-01-28 23:54:26 +0900

    FEAT: Footer Modal 구현

--- selp_fe/src/components/Foot/Menu.js
+++ selp_fe/src/components/Foot/Menu.js
@@ -1,29 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "styles/App.css";
+import MenuItems from "./Modal";
 
 const Menu = () => {
     return (
         <menu>
-            <div className="menu_box">
-                <div className="menu_title">
-                    이용안내
-                    <div className="menu_item">
-                        <Link to="/" className="menu_link">검수기준</Link>
-                    </div>
-                    <div className="menu_item">
-                        <Link to="/" className="menu_link">이용안내</Link>
-                    </div>
-                    <div className="menu_item">
-                        <Link to="/" className="menu_link">패널티 정책</Link>
-                    </div>
-                    <div className="menu_item">
-                        <Link to="/" className="menu_link">커뮤니티 가이드라인</Link>
-                    </div>
-                </div>
-            </div>
+            <div className="menu_title">이용안내</div>
+            <MenuItems />
         </menu>
     );
 };
+
+
 
 export default Menu;
